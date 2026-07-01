15e87a2ce2f7c808d70d43162c5dc5acd65371da
HamedSadim1 <hamid.sadim@outlook.com>
2026-07-01 23:20:37 +0200

    chore: configure ESLint and add lint step to CI/pre-commit
    - Add ESLint v10 flat config for React + TypeScript + Vite
    - Include plugins: react-hooks, react-refresh, typescript-eslint
    - Add lint script to package.json
    - Add lint step to PR workflow and pre-commit hook
    - Fix 11 unused imports/variables across 5 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useReducer } from "react";
 import "./App.css";
-import PostList from "./react-query/PostList";
-import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/counter/Counter";
-import LoginStatus from "./state-management/auth/LoginStatus";
-import TaskList from "./state-management/tasks/TaskList";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
-import TasksContext from "./state-management/tasks/taskContext";
 import { TasksProvider } from "./state-management/tasks";
 
 function App() {
